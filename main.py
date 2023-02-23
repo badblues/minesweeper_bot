ac50319f02889f59b5e56aa7f2c8d48274122cce
badblues <borovikxv.a@gmail.com>
2023-02-23 10:56:25 +0700

    it finally draws rectangles

--- main.py
+++ main.py
@@ -1,50 +1,54 @@
 import sys
-import random
+from random import random
 from PySide6 import QtCore, QtWidgets, QtGui
 from PySide6.QtGui import QPalette, QPainter, QPen, QBrush, QColor
 from PySide6.QtCore import QSize, Slot
 from PySide6.QtWidgets import QStyleOption
 
-class Cell(QtWidgets.QWidget):
-    SIZE = 10
+class Cell:
+    def __init__(self, x, y, mine):
+        self.x = x
+        self.y = y
+        self.mine = mine
 
-    def __init__ (self):
-        super().__init__()
-        self.setGeometry(0, 0, self.SIZE, self.SIZE)
-        self.setFixedSize(QSize(self.SIZE, self.SIZE))
-        self.setStyleSheet("QWidget {color: cyan}")
-        self.repaint()
 
-    def mousePressEvent(self, event):
-        print("event:" + str(event.x()) + " " + str(event.y()))
-
-    def paintEvent(self, e):
-        option = QStyleOption()
-        option.initFrom(self)
-        painter = QPainter(self)
-        painter.end()
-        self.style().drawPrimitive()
-       
 
 
 
 class Field(QtWidgets.QWidget):
+    
     CELL_NUM = 25
+    CELL_SIZE = 10
     
     def __init__ (self):
         super().__init__()
-        self.setGeometry(0, 0, 1000, 1000)
+        self.setGeometry(100, 100, 1000, 1000)
         self.layout = QtWidgets.QGridLayout(self)
         self.cells = []
         for i in range(self.CELL_NUM * self.CELL_NUM):
             x = int(i / self.CELL_NUM)
             y = i % self.CELL_NUM
-            self.cells.append(Cell())
-            self.layout.addWidget(self.cells[i], x, y)
+            isMine = random() > 0.9
+            self.cells.append(Cell(x, y, isMine))
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        rect = self.contentsRect()
+        boardtop = rect.bottom() - self.CELL_SIZE * rect.height() / self.CELL_SIZE
+        for cell in self.cells:
+            color = QColor.fromRgb(255, 0, 0) if cell.mine else QColor.fromRgb(0, 255, 0)
+            self.draw_square(painter, rect.left() + cell.x * self.CELL_SIZE, boardtop + cell.y * self.CELL_SIZE, color)
+        
+
+    def draw_square(self, painter, x, y, color):
+        rect = self.contentsRect()
+        painter.fillRect(x + 1, y + 1, rect.width() / self.CELL_SIZE - 2, rect.height() / self.CELL_SIZE - 2, color)
 
 
+        
 
 
+    
 class Window(QtWidgets.QWidget):
     HEIGHT = 720
     WIDTH = 1280
@@ -72,8 +76,6 @@ class Window(QtWidgets.QWidget):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-
     window = Window() 
     window.show()
-
     sys.exit(app.exec())
