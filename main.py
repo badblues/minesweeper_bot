60dd61245ee3c25dbe85f07fc478df3f8c3d2940
badblues <borovikxv.a@gmail.com>
2023-02-24 11:44:47 +0700

    made mines numbers

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ class Cell:
         self.j = j
         self.mine = mine
         self.opened = False
+        self.mines_around = 0
     
 
 class Field(QtWidgets.QWidget):
     
-    CELL_NUM = 33
+    CELL_NUM = 20
     FIELD_SIZE = 551
     CELL_OFFSET = 1
     CELL_SIZE = (FIELD_SIZE - (CELL_NUM + 1) * CELL_OFFSET) / CELL_NUM
@@ -29,15 +30,10 @@ class Field(QtWidgets.QWidget):
         self.contentsRect().setWidth(self.FIELD_SIZE)
         self.contentsRect().setHeight(self.FIELD_SIZE)
         for i in range(self.CELL_NUM * self.CELL_NUM):
-            x = i % self.CELL_NUM
-            y = int(i / self.CELL_NUM)
             isMine = random() > 0.9
-            self.cells.append(Cell(x, y, isMine))
-        self.cells[0].opened = True
-        self.cells[5].opened = True
-        self.cells[10].opened = True
-        self.cells[15].opened = True
-        self.cells[20].opened = True
+            self.cells.append(Cell(int(i / self.CELL_NUM), i % self.CELL_NUM, isMine))
+        for cell in self.cells:
+            cell.mines_around = self.mines_around(cell)
 
     def paintEvent(self, event):
         rect = self.contentsRect()
@@ -46,27 +42,59 @@ class Field(QtWidgets.QWidget):
         for cell in self.cells:
             self.draw_cell(painter, cell)
         
+    #TODO: better code
     def draw_cell(self, painter, cell):
-        if cell.opened:
-            if cell.mine:
-                color = QColor.fromRgb(255, 0, 0)
-            else:
+        x = cell.j * (self.CELL_SIZE + self.CELL_OFFSET) + self.CELL_OFFSET
+        y = cell.i * (self.CELL_SIZE + self.CELL_OFFSET) + self.CELL_OFFSET
+        if cell.opened & cell.mine:
+            color = QColor.fromRgb(255, 0, 0)
+        elif cell.opened:
                 color = QColor.fromRgb(0, 255, 0)
         else:
             color = QColor.fromRgb(99, 92, 92)
-        x = cell.i * (self.CELL_SIZE + self.CELL_OFFSET) + self.CELL_OFFSET
-        y = cell.j * (self.CELL_SIZE + self.CELL_OFFSET) + self.CELL_OFFSET
         painter.fillRect(x, y, self.CELL_SIZE, self.CELL_SIZE, color)
+        if (cell.opened) & (cell.mine == False):
+            font = painter.font()
+            font.setPixelSize(self.CELL_SIZE)
+            painter.setFont(font)
+            painter.setPen(QColor.fromRgb(0, 0, 0))
+            painter.drawText(x + self.CELL_SIZE * 1/10, y + self.CELL_SIZE * 3/4, str(cell.mines_around))
 
-    def cells_around(self, cell):
+    def mines_around(self, cell):
+        i = cell.i
+        j = cell.j
+        id = i * self.CELL_NUM + j
         num = 0
+        for di in [-1, 0, 1]:
+            for dj in [-1, 0, 1]:
+                neighbour_id = (i + di) * self.CELL_NUM + j + dj 
+                if (neighbour_id >= 0) & (neighbour_id < len(self.cells)):
+                    num += 1 if self.cells[neighbour_id].mine == True else 0
+        return num
 
     def mousePressEvent(self, event):
-        pos_i = int((event.x() - self.CELL_OFFSET) / (self.CELL_SIZE + self.CELL_OFFSET))
-        pos_j = int((event.y() - self.CELL_OFFSET) / (self.CELL_SIZE + self.CELL_OFFSET))
-        i = int(pos_j * self.CELL_NUM + pos_i)
-        print(pos_i, pos_j, i)
-        self.cells[i].opened = True
+        i = int((event.y() - self.CELL_OFFSET) / (self.CELL_SIZE + self.CELL_OFFSET))
+        j = int((event.x() - self.CELL_OFFSET) / (self.CELL_SIZE + self.CELL_OFFSET))
+        id = int(i * self.CELL_NUM + j)
+        self.cells[id].opened = True
+        print(self.cells[id].mines_around)
+        self.repaint()
+
+    #TODO
+    def open_neighbour(self, cell):
+        cell.opened = True
+        if cell.mines_around == 0:
+            i = cell.i
+            j = cell.j
+            if self.get_cell(i - 1, j):
+                self.open_neighbour(self.cells[cell.j - 1])
+    #TODO
+    def get_cell(self, i, j):
+        return self.cells[j * self.CELL_NUM + i]
+
+    def open_all(self):
+        for cell in self.cells:
+            cell.opened = True
         self.repaint()
         
 
@@ -94,6 +122,7 @@ class Window(QtWidgets.QWidget):
 
     @Slot()
     def foo(self):
+        self.field.open_all()
         print("function doing something")
 
 
